fcb73c69711caec29bebf8ee188527fd94d279d6
OriGamito <orisan618@gmail.com>
2024-07-23 15:13:34 -0600

    Modificaciones a la vista y funcionalidad de productos

--- src/productos.php
+++ src/productos.php
@@ -8,66 +8,53 @@ $existe = mysqli_fetch_all($sql);
 if (empty($existe) && $id_user != 1) {
     header('Location: permisos.php');
 }
+
 if (!empty($_POST)) {
     $alert = "";
     $id = $_POST['id'];
-    $codigo = $_POST['codigo'];
     $producto = $_POST['producto'];
     $precio = $_POST['precio'];
     $cantidad = $_POST['cantidad'];
-    if (empty($codigo) || empty($producto) || empty($precio) || $precio <  0 || empty($cantidad) || $cantidad <  0) {
-        $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                        Todo los campos son obligatorios
+
+    switch (true) {
+        case empty($producto) || empty($precio) || $precio < 0 || empty($cantidad) || $cantidad < 0:
+            $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        Todos los campos son obligatorios
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>';
-    } else {
-        if (empty($id)) {
-            $query = mysqli_query($conexion, "SELECT * FROM producto WHERE codigo = '$codigo'");
+            break;
+
+        case empty($producto):
+            $query = mysqli_query($conexion, "SELECT * FROM producto WHERE descripcion = '$producto'");
             $result = mysqli_fetch_array($query);
             if ($result > 0) {
                 $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                        El codigo ya existe
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>';
+                            El Producto ya existe
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>';
             } else {
-                $query_insert = mysqli_query($conexion, "INSERT INTO producto(codigo,descripcion,precio,existencia) values ('$codigo', '$producto', '$precio', '$cantidad')");
+                $query_insert = mysqli_query($conexion, "INSERT INTO producto(descripcion,precio,existencia) values ('$producto', '$precio', '$cantidad')");
                 if ($query_insert) {
                     $alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                        Producto registrado
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>';
+                                Producto registrado
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>';
                 } else {
                     $alert = '<div class="alert alert-danger" role="alert">
-                    Error al registrar el producto
-                  </div>';
+                                Error al registrar el producto
+                              </div>';
                 }
             }
-        } else {
-            $query_update = mysqli_query($conexion, "UPDATE producto SET codigo = '$codigo', descripcion = '$producto', precio= $precio, existencia = $cantidad WHERE codproducto = $id");
-            if ($query_update) {
-                $alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                        Producto Modificado
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>';
-            } else {
-                $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                        Error al modificar
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>';
-            }
-        }
+            break;
     }
 }
+
 include_once "includes/header.php";
 ?>
 <div class="card shadow-lg">
@@ -77,13 +64,6 @@ include_once "includes/header.php";
                 <form action="" method="post" autocomplete="off" id="formulario">
                     <?php echo isset($alert) ? $alert : ''; ?>
                     <div class="row">
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label for="codigo" class=" text-dark font-weight-bold"><i class="fas fa-barcode"></i> Código de Barras</label>
-                                <input type="text" placeholder="Ingrese código de barras" name="codigo" id="codigo" class="form-control">
-                                <input type="hidden" id="id" name="id">
-                            </div>
-                        </div>
                         <div class="col-md-5">
                             <div class="form-group">
                                 <label for="producto" class=" text-dark font-weight-bold">Producto</label>
@@ -104,7 +84,6 @@ include_once "includes/header.php";
                         </div>
                         <div class="col-md-6">
                             <input type="submit" value="Registrar" class="btn btn-primary" id="btnAccion">
-                            <input type="button" value="Nuevo" onclick="limpiar()" class="btn btn-success" id="btnNuevo">
                         </div>
                     </div>
 
@@ -117,7 +96,6 @@ include_once "includes/header.php";
                     <thead class="thead-dark">
                         <tr>
                             <th>#</th>
-                            <th>Código</th>
                             <th>Producto</th>
                             <th>Precio</th>
                             <th>Stock</th>
@@ -134,16 +112,12 @@ include_once "includes/header.php";
                             while ($data = mysqli_fetch_assoc($query)) { ?>
                                 <tr>
                                     <td><?php echo $data['codproducto']; ?></td>
-                                    <td><?php echo $data['codigo']; ?></td>
                                     <td><?php echo $data['descripcion']; ?></td>
                                     <td><?php echo $data['precio']; ?></td>
-                                    <td><?php echo $data['existencia']; ?></td>
+                                    <td id="stock_<?php echo $data['codproducto']; ?>"><?php echo $data['existencia']; ?></td>
                                     <td>
-                                        <a href="#" onclick="editarProducto(<?php echo $data['codproducto']; ?>)" class="btn btn-primary"><i class='fas fa-edit'></i></a>
-
-                                        <form action="eliminar_producto.php?id=<?php echo $data['codproducto']; ?>" method="post" class="confirmar d-inline">
-                                            <button class="btn btn-danger" type="submit"><i class='fas fa-trash-alt'></i> </button>
-                                        </form>
+                                        <button class="btn btn-success btn-sm" onclick="actualizarStock(<?php echo $data['codproducto']; ?>, 'mas')">+</button>
+                                        <button class="btn btn-danger btn-sm" onclick="actualizarStock(<?php echo $data['codproducto']; ?>, 'menos')">-</button>
                                     </td>
                                 </tr>
                         <?php }
@@ -156,3 +130,24 @@ include_once "includes/header.php";
     </div>
 </div>
 <?php include_once "includes/footer.php"; ?>
+
+<script>
+    function actualizarStock(idProducto, accion) {
+        $.ajax({
+            url: 'actualizar_stock.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                id: idProducto,
+                accion: accion
+            },
+            success: function(response) {
+                // Actualizar el stock en la tabla
+                $('#stock_' + idProducto).text(response.nuevoStock);
+            },
+            error: function(error) {
+                console.error('Error en la solicitud AJAX: ', error);
+            }
+        });
+    }
+</script>
